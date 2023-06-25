f1b2fc208df74fa010edb97766195ca1514524ef
matthewsciamanna <136662857+matthewsciamanna@users.noreply.github.com>
2023-06-25 15:47:15 -0400

    Update script.js
    upload and view pdf

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // comment
+document.getElementById('upload-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  var fileInput = document.getElementById('pdf-upload');
+  var file = fileInput.files[0];
+  var reader = new FileReader();
+
+  reader.onload = function (e) {
+    var pdfUrl = e.target.result;
+    var listItem = document.createElement('li');
+    var embedElement = document.createElement('embed');
+    embedElement.src = pdfUrl;
+    embedElement.type = 'application/pdf';
+    listItem.appendChild(embedElement);
+
+    document.getElementById('pdf-items').appendChild(listItem);
+  };
+
+  reader.readAsDataURL(file);
+});
