903e573eb9e52e4922da948c8694115840b6f896
matthewsciamanna <136662857+matthewsciamanna@users.noreply.github.com>
2023-06-25 16:06:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,40 +4,30 @@ document.getElementById('upload-form').addEventListener('submit', function (e) {
   var fileInput = document.getElementById('pdf-upload');
   var file = fileInput.files[0];
 
-  // Upload the file to the server and obtain the file URL
-  uploadFile(file)
-    .then(function (url) {
-      var listItem = document.createElement('li');
-      var embedElement = document.createElement('embed');
-      embedElement.src = url;
-      embedElement.type = 'application/pdf';
-      listItem.appendChild(embedElement);
+  var formData = new FormData();
+  formData.append('pdf', file);
 
-      document.getElementById('pdf-items').appendChild(listItem);
-    })
-    .catch(function (error) {
-      console.error('Error uploading the file:', error);
-    });
-});
+  fetch('/upload', {
+    method: 'POST',
+    body: formData
+  })
+  .then(function (response) {
+    if (response.ok) {
+      return response.text();
+    } else {
+      throw new Error('Error uploading the file.');
+    }
+  })
+  .then(function (url) {
+    var listItem = document.createElement('li');
+    var embedElement = document.createElement('embed');
+    embedElement.src = url;
+    embedElement.type = 'application/pdf';
+    listItem.appendChild(embedElement);
 
-function uploadFile(file) {
-  // Replace this code with your server-side implementation for file upload
-  // and return the file URL
-
-  return new Promise(function (resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/upload'); // Replace '/upload' with your server endpoint for file upload
-    xhr.setRequestHeader('Content-Type', 'application/pdf');
-    xhr.onload = function () {
-      if (xhr.status === 200) {
-        resolve(xhr.responseText);
-      } else {
-        reject(xhr.statusText);
-      }
-    };
-    xhr.onerror = function () {
-      reject(xhr.statusText);
-    };
-    xhr.send(file);
+    document.getElementById('pdf-items').appendChild(listItem);
+  })
+  .catch(function (error) {
+    console.error('Error:', error);
   });
-}
+});
